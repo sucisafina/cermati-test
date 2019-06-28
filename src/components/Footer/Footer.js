14cd77d66f13fee4da74c2610a30c8ddce39ffa8
Janto Andriano <38934947+JantoAndriano@users.noreply.github.com>
2019-06-28 08:47:39 +0700

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,18 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './styles.css'
 import Emailbar from '../Emailbar/Emailbar';
-
+import { faCopyright } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
 
     return (
         <>
             <div className='footer'>
-                <div className='footer-item'>@2018 Janto Motulo. All rights reserved</div>
-                
+                <div className='footer-item'>
+                    <FontAwesomeIcon icon={faCopyright}/> 2018 Janto Motulo. All rights reserved
+                </div> 
             </div>
-           
            <Emailbar/>
         </>
     )
